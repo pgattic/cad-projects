59a8e7660da4713f77ed031f074a9c1c079d5ff9
Preston Corless <pgattic@gmail.com>
2026-09-09 13:17:06 -0600

    Update inputs, nixpkgs -> channels.nixos.org

--- flake.nix
+++ flake.nix
@@ -2,18 +2,18 @@
   description = "CAD Projects Nix Flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.zst";
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
   outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-    systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+    systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ];
 
     perSystem = { pkgs, ... }: {
       devShells.default = pkgs.mkShell {
-        packages = with pkgs; [
-          openscad
-          openscad-lsp
+        packages = [
+          pkgs.openscad
+          pkgs.openscad-lsp
         ];
       };
     };
